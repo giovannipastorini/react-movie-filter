df6ed7627ca8885ba7e0354d1649f71f8f63448a
giovannipastorini <giovannipastorini9@gmail.com>
2026-05-12 20:00:38 +0200

    Implement movie filtering by title and genre in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,69 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function App() {
   
 
   const initialMovies=[
-  { id: 1, title: 'Inception', genre: 'Fantascienza' },
-  { id: 2, title: 'Il Padrino', genre: 'Thriller' },
-  { id: 3, title: 'Titanic', genre: 'Romantico' },
-  { id: 4, title: 'Batman', genre: 'Azione' },
-  { id: 5, title: 'Interstellar', genre: 'Fantascienza' },
-  { id: 6, title: 'Pulp Fiction', genre: 'Thriller' },
-]
+    { id: 1, title: 'Inception', genre: 'Fantascienza' },
+    { id: 2, title: 'Il Padrino', genre: 'Thriller' },
+    { id: 3, title: 'Titanic', genre: 'Romantico' },
+    { id: 4, title: 'Batman', genre: 'Azione' },
+    { id: 5, title: 'Interstellar', genre: 'Fantascienza' },
+    { id: 6, title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
 
-//Fantascienza, Thriller, Romantico, Azione, Fantascienza, Thriller
+  const [movies, setMovies] = useState(initialMovies)
+  const [genere, setGenere]=useState('')
+  const [search, setSearch] = useState('')
 
-const [movies, setMovies] = useState(initialMovies)
+  useEffect(() => {
+    console.log(genere);
+
+    
+    let filteredData = initialMovies;
+    // filter data by genre
+    if (genere) {
+      //console.log('Filter the data by genre');
+      filteredData = initialMovies.filter(movie => movie.genre.toLowerCase() == genere.toLowerCase())
+    }
+
+    if(search){
+      console.log('the user searched:', search);
+      filteredData = filteredData.filter(movie => movie.title.toLowerCase().includes(search.toLowerCase()))
+     
+    }
+
+    console.log(filteredData);
+    setMovies(filteredData);
+
+  }, [genere, search])
 
   return (
     <>
 
-    <div>
-      <select>
-        <option value="Fantascienza">Fantascienza</option>
-        <option value="Thriller">Thriller</option>
-        <option value="Romantico">Romantico</option>
-        <option value="Azione">Azione</option>
-        <option value="Fantascienza">Fantascienza</option>
-        <option value="Thriller">Thriller</option>
-      </select>
-    </div>
-    
+    {/* Filter by Title */}
+      <div>
+          <label htmlFor="title">Title</label><br />
+          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search a movie by title" />
+      </div>
+
+
+    {/* Filter by Genre */}
+      <div>
+
+        <label htmlFor="genre">Genre</label><br />
+        <select name="genre" id="genre" value={genere} onChange={(e) => setGenere(e.target.value)}>
+          <option value="">All</option>
+          <option value="fantascienza">Fantascienza</option>
+          <option value="thriller">Thriller</option>
+          <option value="romantico">Romantico</option>
+          <option value="azione">Azione</option>
+        </select>
+      </div>
+
+
+    {/* Movies List */}
     {
       movies.map( movie =>(
         <div key={movie.id}>
